17ea68171cdce9efbf1b1c7549da3e8f3258be94
Varshith-Yadav <varshithlavanya67@gmail.com>
2026-02-10 03:15:04 +0530

    changed to ESM

--- backend/store-plartform/src/k8s/client.ts
+++ backend/store-plartform/src/k8s/client.ts
@@ -1,12 +1,12 @@
-let coreV1Api: any;
+let k8s: typeof import("@kubernetes/client-node");
 
 export async function getCoreV1Api() {
-  if (coreV1Api) return coreV1Api;
-  
-  const k8s = await import("@kubernetes/client-node");
+  if (!k8s) {
+    k8s = await import("@kubernetes/client-node");
+  }
+
   const kc = new k8s.KubeConfig();
   kc.loadFromDefault();
-  coreV1Api = kc.makeApiClient(k8s.CoreV1Api);
-  
-  return coreV1Api;
+
+  return kc.makeApiClient(k8s.CoreV1Api);
 }
