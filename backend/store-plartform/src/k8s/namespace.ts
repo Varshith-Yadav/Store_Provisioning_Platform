616a7ce70f7e35d6c639459f2f3a3e3758b00d7d
Varshith-Yadav <varshithlavanya67@gmail.com>
2026-02-10 03:18:09 +0530

    changed

--- backend/store-plartform/src/k8s/namespace.ts
+++ backend/store-plartform/src/k8s/namespace.ts
@@ -1,26 +1,25 @@
-import { getCoreV1Api }  from "./client";
+import { getCoreV1Api } from "./client";
 
 
-export async function createNamespace(namespace: string, labels: Record<string, string>){
+export async function createNamespace(namespace: string, labels: Record<string, string>) {
     try {
+        // Create the namespace
         const coreV1Api = await getCoreV1Api();
-        await coreV1Api.createNamespace(
-            {
-                body: {
-                    metadata: {
-                        name: namespace,
-                        labels
-                    }
+        await coreV1Api.createNamespace({
+            body: {
+                metadata: {
+                    name: namespace,
+                    labels
                 }
             }
-        );
+        });
 
         console.log(`Namespace ${namespace} created successfully`);
     }
-    catch (err: any){
-        if (err.response?.statusCode === 409){
+    catch (err: any) {
+        if (err.response?.statusCode === 409) {
             console.log(`Namespace ${namespace} already exits`);
-            return ;
+            return;
         }
         throw err;
     }
